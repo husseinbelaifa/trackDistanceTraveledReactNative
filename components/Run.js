5b7e1993cf9d9411dc2beb601d6919a401013bc4
belaifa el hussein <belaifahocine@gmail.com>
2019-06-30 15:40:54 +0200

    set up polyline & marker

--- components/Run.js
+++ components/Run.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
 import { MapView, Location } from "expo";
+
+const { Marker, Polyline } = MapView;
+
 export default class Run extends Component {
   state = {
     positions: []
@@ -25,9 +28,52 @@ export default class Run extends Component {
     this.setState({ positions: [...this.state.positions, position] });
   };
   render() {
+    const { latitude, longitude } = this.props;
+    const currentPosition =
+      this.state.positions.length === 0
+        ? {
+            coords: {
+              latitude: this.props.latitude,
+              longitude: this.props.longitude
+            }
+          }
+        : {
+            coords: {
+              latitude: this.state.positions[this.state.positions.length - 1]
+                .coords.latitude,
+              longitude: this.state.positions[this.state.positions.length - 1]
+                .coords.longitude
+            }
+          };
+
+    console.log(currentPosition);
     return (
       <View style={styles.container}>
-        <MapView style={styles.map} provider="google" />
+        <MapView
+          style={styles.map}
+          provider="google"
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.01
+          }}
+        >
+          <Marker coordinate={currentPosition && currentPosition.coords} />
+          <Polyline
+            coordinates={
+              this.state.positions &&
+              this.state.positions.map(position => {
+                return {
+                  latitude: position.coords.latitude,
+                  longitude: position.coords.longitude
+                };
+              })
+            }
+            strokeWidth={10}
+            strokeColor="#f2b659"
+          />
+        </MapView>
       </View>
     );
   }
